1e320644a79bbc4e036ff9f210918b6ec376c515
ArilsonFJS <arilsonsilva61@gmail.com>
2023-02-26 18:02:15 -0300

    commit inicial

---
+++ filmes-populares/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  filmes-populares
+//
+//  Created by Arilson Silva on 26/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
