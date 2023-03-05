6413af9ec6bd71b9ce418e1c3b5cd41bd5f24a69
ArilsonFJS <arilsonsilva61@gmail.com>
2023-03-04 21:25:10 -0300

    definindo as constraints de titleView e TableView

--- filmes-populares/HomeViewController.swift
+++ filmes-populares/HomeViewController.swift
@@ -19,7 +19,7 @@ class HomeViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Filmes Populares"
-        label.textColor = .white
+        //label.textColor = .white
         label.font = .systemFont(ofSize: 24.0, weight: .bold)
         
         return label
@@ -38,7 +38,43 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
         
+    }
+    
+    
+    private func setLayout(){
+        view.addSubview(titleView)
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            tableView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 32.0),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+                ])
+
+    }
+}
+
+
+
+extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let filme = filmes[indexPath.row]
+        cell.textLabel?.text = filme.title
+        cell.backgroundColor = .clear
+        //cell.textLabel?.textColor = .white
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filmes.count
     }
     
     /*func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -60,24 +96,5 @@ class HomeViewController: UIViewController {
             }
         }
     }*/
-}
-
-
-
-extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filmes.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = filmes[indexPath.row].title
-        cell.backgroundColor = .clear
-        cell.textLabel?.textColor = .white
-        
-        return cell
-    }
-    
-    
 }
 
